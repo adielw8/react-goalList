d087a18c9837d444409fcb4c94926b043bf73a99
adielw8 <adielw8@gmail.com>
2021-03-18 12:42:07 +0200

    first commit

---
+++ src/components/GoalList/GoalList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './GoalList.css'
+
+const GoalList = () => {
+  return <div className="goals">
+      <li className="goalList">goal 1</li>
+      <li className="goalList">goal 2</li>
+      <li className="goalList">goal 3</li>
+  </div>
+  
+}
+
+export default GoalList;
